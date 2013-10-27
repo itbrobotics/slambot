c9e9f1f079350801d4a251464fc30957db8cb844
swordmaster2k <b00056835@student.itb.ie>
2013-10-27 14:39:45 +0000

    Initial commit, with adafruit motor controller code

---
+++ motor/motor.h
@@ -0,0 +1,115 @@
+/**
+* File: motor.h
+*
+* Defines the basic motor functionality for SLAM Robot built 
+* using the adafruit motor shield. The robot contains 4
+* standard DC motors.
+*
+* NOTE: Figure out why uncommenting turnRight and turnLeft yields a 
+* 'boolean' does not name a type error! Seems that you cannot
+* return type boolean from a function contained within a header file 
+* in Arduino C?!?
+*
+* @author Joshua Michael Daly
+* @version 27/10/2013
+*/
+
+/**
+* File: motor.ino
+*
+* Provides basic motor functionality for SLAM Robot built 
+* using the adafruit motor shield. The robot contains 4
+* standard DC motors.
+*
+* It is possible to drive forward and backwards, turn left and 
+* turn right when travelling forward or backwards. All of these 
+* actions can be run for an optional set duration or for an
+* indefinite provide of time.
+*
+* @author Joshua Michael Daly
+* @version 27/10/2013
+*/
+
+#include <AFMotor.h>
+
+#define MOTOR_1_SPEED 200
+#define MOTOR_2_SPEED 200
+#define MOTOR_3_SPEED 200
+#define MOTOR_4_SPEED 200
+
+/*
+* Create the 4 motors on the rover, 64KHZ PWN on motor 1 and 2 
+* uses more power but generates less electrical noise. Motors 
+* 3 and 4 will only run a 1KHZ and will ignore any setting 
+* given.
+*/
+AF_DCMotor motor1(1, MOTOR12_64KHZ); 
+AF_DCMotor motor2(2, MOTOR12_64KHZ); 
+AF_DCMotor motor3(3); 
+AF_DCMotor motor4(4);
+
+/************************************************************
+* Motor Functions
+************************************************************/
+
+/**
+* Sets the speed for all 4 motors.
+*/
+void setMotorSpeeds();
+
+/**
+* Releases all 4 motors.
+*/
+void releaseAllMotors();
+
+/**
+* Drives all 4 motors forward.
+*/
+void driveForward();
+
+/**
+* Drives all 4 Motors forward for the duration in milliseconds.
+*
+* @param duration time to drive forwards for in milliseconds
+*/
+void driveForward(unsigned long duration);
+
+/**
+* Drives all 4 Motors backward.
+*/
+void driveBackward();
+
+/**
+* Drives all 4 Motors backward for the duration in milliseconds.
+*
+* @param duration duration time to drive backwards for in milliseconds
+*/
+void driveBackward(unsigned long duration);
+
+// N.B. Uncommenting this declartion yields a 'boolean' does not name a type error
+//
+///**
+//* Turns the robot right using motors 1 and 2.
+//*/
+//boolean turnRight(unsigned short path);
+
+/**
+* Turns the robot right using motors 1 and 2 for the duration in milliseconds.
+*
+* @param duration duration time to turn right for in milliseconds
+*/
+void turnRight(unsigned short path, unsigned long duration);
+
+// N.B. Uncommenting this declartion yields a 'boolean' does not name a type error
+//
+///**
+//* Turns the robot left using motors 3 and 4.
+//*/
+//boolean turnLeft(unsigned short path);
+
+/**
+* Turns the robot left using motors 3 and 4 for the duration in milliseconds.
+*
+* @param duration duration time to turn left for in milliseconds
+*/
+void turnLeft(unsigned short path, unsigned long duration);
---
+++ motor/motor.ino
@@ -0,0 +1,219 @@
+/**
+* File: motor.ino
+*
+* Provides basic motor functionality for SLAM Robot built 
+* using the adafruit motor shield. The robot contains 4
+* standard DC motors.
+*
+* It is possible to drive forward and backwards, turn left and 
+* turn right when travelling forward or backwards. All of these 
+* actions can be run for an optional set duration or for an
+* indefinite provide of time.
+*
+* @author Joshua Michael Daly
+* @version 27/10/2013
+*/
+
+#include <AFMotor.h>
+#include "motor.h"
+
+/************************************************************
+* System Functions
+************************************************************/
+
+void setup() 
+{
+  Serial.begin(9600); // set up Serial library at 9600 bps
+  Serial.println("Motor test!");
+  
+  setMotorSpeeds();
+}
+
+void loop() 
+{
+  driveForward();
+  delay(1000);
+  
+  driveBackward();
+  delay(1000);
+  
+  turnRight(FORWARD);
+  delay(1000);
+  
+  turnRight(BACKWARD);
+  delay(1000);
+  
+  turnLeft(FORWARD);
+  delay(1000);
+  
+  turnLeft(BACKWARD);
+  delay(1000);
+  
+  driveForward(2000);
+  
+  driveBackward(2000);
+  
+  turnRight(FORWARD, 2000);
+  turnRight(BACKWARD, 2000);
+  
+  turnLeft(FORWARD, 2000);
+  turnLeft(BACKWARD, 2000);
+  
+  releaseAllMotors();
+  delay(5000);
+}
+
+/************************************************************
+* Motor Functions
+************************************************************/
+
+/**
+* Sets the speed for all 4 motors.
+*/
+void setMotorSpeeds()
+{
+  motor1.setSpeed(MOTOR_1_SPEED);
+  motor2.setSpeed(MOTOR_2_SPEED);
+  motor3.setSpeed(MOTOR_3_SPEED);
+  motor4.setSpeed(MOTOR_4_SPEED);
+}
+
+/**
+* Releases all 4 motors.
+*/
+void releaseAllMotors()
+{
+  motor1.run(RELEASE);
+  motor2.run(RELEASE);
+  motor3.run(RELEASE);
+  motor4.run(RELEASE);
+}
+
+/**
+* Drives all 4 motors forward.
+*/
+void driveForward()
+{
+  motor1.run(FORWARD);
+  motor2.run(FORWARD);
+  motor3.run(FORWARD);
+  motor4.run(FORWARD);
+}
+
+/**
+* Drives all 4 Motors forward for the duration in milliseconds.
+*
+* @param duration time to drive forwards in milliseconds
+*/
+void driveForward(unsigned long duration)
+{
+  driveForward();
+  delay(duration);
+  releaseAllMotors();
+}
+
+/**
+* Drives all 4 Motors backward.
+*/
+void driveBackward()
+{
+  motor1.run(BACKWARD);
+  motor2.run(BACKWARD);
+  motor3.run(BACKWARD);
+  motor4.run(BACKWARD);
+}
+
+/**
+* Drives all 4 Motors backward for the duration in milliseconds.
+*
+* @param duration duration time to drive backwards in milliseconds
+*/
+void driveBackward(unsigned long duration)
+{
+  driveBackward();
+  delay(duration);
+  releaseAllMotors();
+}
+
+/**
+* Turns the robot right using motors 1 and 2. Shuts motors 3 
+* and 4 down.
+*/
+boolean turnRight(unsigned short path)
+{
+  if (path != FORWARD && path != BACKWARD)
+  {
+    Serial.println("Invalid path: " + path);
+    
+    return false; 
+  }
+  
+  // May need to check state of these and enclose in "if".
+  motor3.run(RELEASE);
+  motor4.run(RELEASE);
+  
+  motor1.run(path);
+  motor2.run(path);
+  
+  return true;
+}
+
+/**
+* Turns the robot right using motors 1 and 2 for the duration in milliseconds.
+*
+* @param duration duration time to turn right in milliseconds
+*/
+void turnRight(unsigned short path, unsigned long duration)
+{
+  boolean result = turnRight(path);
+  Serial.println(result);
+  
+  if (result)
+  {
+    delay(duration);
+    motor1.run(RELEASE);
+    motor2.run(RELEASE); 
+  }
+}
+
+/**
+* Turns the robot left using motors 3 and 4. Shuts motors 1
+* and 2 down.
+*/
+boolean turnLeft(unsigned short path)
+{
+  if (path != FORWARD && path != BACKWARD)
+  {
+    Serial.println("Invalid path: " + path);
+    
+    return false; 
+  }
+  
+  // May need to check state of these and enclose in "if".
+  motor1.run(RELEASE);
+  motor2.run(RELEASE);
+  
+  motor3.run(path);
+  motor4.run(path);
+  
+  return true;
+}
+
+/**
+* Turns the robot left using motors 3 and 4 the duration in milliseconds.
+*
+* @param duration duration time to turn left for in milliseconds
+*/
+void turnLeft(unsigned short path, unsigned long duration)
+{
+  boolean result = turnLeft(path);
+  Serial.println(result);
+  
+  if (result)
+  {
+    delay(duration);
+    motor3.run(RELEASE);
+    motor4.run(RELEASE); 
+  }
+}
+
