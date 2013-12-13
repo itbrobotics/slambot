be1657ac63f4fd0faa73b9d08a5b05e254228da7
swordmaster2k <b00056835@student.itb.ie>
2013-12-13 08:36:24 +0000

    + Added simple stop/start command line control.

--- Robot/Robot.ino
+++ Robot/Robot.ino
@@ -1,14 +1,14 @@
 /**
 * File: Robot.ino
 * 
-* This sketch defines the behavior for a "naive" random wandering robot.
+* This sketch defines the behavior for a "naïve" random wandering robot.
 * It is simply capable of progressing forward until it encounters an
-* object within a pre-defined distance, then using a servo mounted
-* sensor it choose to turn left or right. It then continues progressing
+* object at a pre-defined distance, then using a servo mounted sensor 
+* it chooses to turn left or right. It then continues progressing
 * forward if able.
 * 
 * @author Joshua Michael Daly
-* @version 04/12/2013
+* @version 12/12/2013
 */
 
 #include <Servo.h>
@@ -23,81 +23,112 @@ SharpIR rangeFinder;
 MotorController motorController;
 ServoController servoController;
 
+boolean isStopped;
 int nearestObject;
 
 /************************************************************
-* System Functions
+* Arduino Functions
 ************************************************************/
 
 void setup()
 {
-  #ifdef DEBUG
-    Serial.begin(9600);
-  #endif
-  
+  Serial.begin(9600);
+
   rangeFinder.setPin(5);
   servoController.setPin(10);
+  
+  isStopped = true; // Ensure the robot is stopped on start up.
 }
 
 void loop()
 { 
-  nearestObject = 0;
-  
-  // Don't accept a negative value for the distance.
-  while (nearestObject <= 0)
+  if (Serial.available()) // Check to see if at least one character is available.
   {
-    nearestObject += rangeFinder.getDistance();
-  }     
-
-  #if DEBUG
-    Serial.print("Distance to object: ");
-    Serial.print(nearestObject);
-    Serial.println("cm");
-  #endif
-  
-  if (nearestObject > 30) // Object is further than 30cm away.
-  {
-    motorController.driveForward();
+    char character = Serial.read();
     
-    #if !DEBUG
-      // Give the Sharp sensor time to respond, this is not
-      // needed when sending serial data because of the
-      // delay created by those functions.
-      delay(10); 
-    #endif
-  }
-  else
-  {
-    motorController.releaseAllMotors();
-    motorController.driveBackward(500);
-    
-    int distanceLeft = lookLeft();
-    int distanceRight = lookRight();
-    
-    #if DEBUG
-      Serial.print("Left Distance: ");
-      Serial.print(distanceLeft);
-      Serial.println("cm");
-      Serial.print("Right Distance: ");
-      Serial.print(distanceRight);
-      Serial.println("cm");
-    #endif
-    
-    if (distanceLeft > distanceRight)
+    switch (tolower(character))
     {
-      #if DEBUG
-        Serial.println("Turning Left!");
-      #endif
+      case 'q':
+        motorController.releaseAllMotors();
+        isStopped = true;
+        #if DEBUG
+          Serial.println("Stop");
+        #endif
+        break;
+      case 'e':
+        isStopped = false;
+        #if DEBUG
+          Serial.println("Start");
+        #endif
+        break;
+      default: 
+        Serial.print("Unknown command \"");
+        Serial.print(character);
+        Serial.println("\"");
+        break;
+    }  
+  }
+  
+  if (!isStopped)
+  {
+    nearestObject = 0;
+    
+    // Don't accept a negative value for the distance.
+    while (nearestObject <= 0)
+    {
+      nearestObject += rangeFinder.getDistance();
+    }     
+  
+    #if DEBUG
+      Serial.print("Distance to object: ");
+      Serial.print(nearestObject);
+      Serial.println("cm");
+    #endif
+    
+    if (nearestObject > 30) // Object is further than 30cm away.
+    {
+      motorController.driveForward();
       
-      motorController.turnLeft(FORWARD, 1500);
+      #if !DEBUG
+        // Give the Sharp sensor time to respond, this is not
+        // needed when sending serial data because of the
+        // delay created by those functions.
+        delay(10); 
+      #endif
     }
     else
     {
+      motorController.releaseAllMotors();
+      motorController.driveBackward(500);
+      
+      int distanceLeft = lookLeft();
+      int distanceRight = lookRight();
+      
       #if DEBUG
-        Serial.println("Turning Right!");
+        Serial.print("Left Distance: ");
+        Serial.print(distanceLeft);
+        Serial.println("cm");
+        Serial.print("Right Distance: ");
+        Serial.print(distanceRight);
+        Serial.println("cm");
       #endif
       
-      motorController.turnRight(FORWARD, 1500);
+      if (distanceLeft > distanceRight)
+      {
+        #if DEBUG
+          Serial.println("Turning Left!");
+        #endif
+        
+        motorController.turnLeft(FORWARD, 1500);
+      }
+      else
+      {
+        #if DEBUG
+          Serial.println("Turning Right!");
+        #endif
+        
+        motorController.turnRight(FORWARD, 1500);
+      }
     }
   }
 }
