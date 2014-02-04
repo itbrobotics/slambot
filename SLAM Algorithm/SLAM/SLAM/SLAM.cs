07b8eaf5886f1fae770b7bf37409f8eb1712ca6f
swordmaster2k <b00056835@student.itb.ie>
2014-02-04 08:32:40 +0000

    * Some modifications to the SLAM algorithm.

--- SLAM Algorithm/SLAM/SLAM/SLAM.cs
+++ SLAM Algorithm/SLAM/SLAM/SLAM.cs
@@ -5,8 +5,6 @@ namespace SLAM
 {
 	public class SLAM
 	{
-		private const int DegreesPerScan = 1;
-
 		public SLAM ()
 		{
 			
@@ -14,26 +12,6 @@ namespace SLAM
 
 		public static void Main (String[] args)
 		{
-			/*
-			 * Step 1: 	Create a Landmarks object set to 1 
-			 * 			degree per scan.
-			 * 
-			 * Step 2: 	Collect laser and robot position data 
-			 * 		   	at the same time.
-			 * 
-			 * Step 3: 	Extract the line landmarks from the
-			 * 		   	data.
-			 * 
-			 * Step 4:	Update the landmarks database with
-			 * 			the new landmarks.
-			 * 
-			 * Step 5:	Remove any bad landmarks or doubles
-			 * 			from the database.
-			 * 
-			 * Step 6:	Display the results in some form.
-			 * 
-			 * Step 7:	Repeat steps 2-6 for a finite time.
-			 */
 			Application.Init ();
 			new MapView (new Map ());
 			Application.Run ();
